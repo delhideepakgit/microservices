c3a8be873e92b5a85b3fd02fb3aa4d3c63a2b2a8
abc <abc@MININT-AEVSPO0>
2020-04-11 13:18:34 +0530

    Added LimitsConfiguration

---
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,17 @@
+package com.in28minutes.microservices.limitsservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.microservices.limitsservice.bean.LimitsConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@GetMapping("/limits")
+	public LimitsConfiguration retrieveLimitsFromConfiguration() {
+		
+		return new LimitsConfiguration(1000, 1);
+	}
+
+}
