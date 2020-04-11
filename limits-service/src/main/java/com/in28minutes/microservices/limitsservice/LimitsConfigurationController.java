6a25d3ea4cc4bc69bc6d3602a8bea5a8e7ffde82
abc <abc@MININT-AEVSPO0>
2020-04-11 13:24:52 +0530

    Enhance limits service to get configuration from application.properties

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,14 @@ import com.in28minutes.microservices.limitsservice.bean.LimitsConfiguration;
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public LimitsConfiguration retrieveLimitsFromConfiguration() {
 		
-		return new LimitsConfiguration(1000, 1);
+		return new LimitsConfiguration(configuration.getMaximum(), 
+				configuration.getMinimum());
 	}
 
 }
